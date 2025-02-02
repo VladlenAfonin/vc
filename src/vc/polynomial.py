5bb953ad1006edf825fc09615d923cedc68b4617
vladlenafonin <vladlenafonin.university@yandex.ru>
2025-02-02 23:25:19 +0300

    chore: add comment

--- src/vc/polynomial.py
+++ src/vc/polynomial.py
@@ -6,5 +6,6 @@ def polynomial_quotient(g: galois.Poly, xs: galois.Array) -> galois.Poly:
 
 
 def degree_correct(g: galois.Poly, r: int, n: int) -> galois.Poly:
+    # Generate polynomial 1 + rx + (rx)^2 + ... + (rx)^n.
     random_polynomial = galois.Poly([r ** power for power in range(n + 1)], field=g.field)
     return g * random_polynomial
